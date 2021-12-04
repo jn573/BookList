458cf4e77980c1d32ce874053a22bf8b7c0f1517
James <cheekywena@gmail.com>
2021-12-04 11:42:53 +0200

    lesson 51

---
+++ booklist/app1.js
@@ -0,0 +1,65 @@
+// book constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// ui constructor
+UI.prototype.addBookToList = function (book) {
+
+    const list = document.getElementById('book-list')
+    // create TR
+    const row = document.createElement('tr')
+    const saveToLs = function () {
+        sessionStorage.setItem(book.isbn, JSON.stringify(book.title, book.author, book.isbn))
+    }
+    row.innerHTML =
+        `<td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td>Success</td>`
+    list.appendChild(row)
+    saveToLs()
+}
+
+function UI() {
+    const addBookToLs = function addBookToLs(title, author, isbn) {
+        sessionStorage.setItem('book', JSON.stringify(title, author, isbn));
+    }
+
+
+    UI.prototype.clearFields = function () {
+        document.getElementById('title').value = ""
+        document.getElementById('author').value = ""
+        document.getElementById('value').value = ""
+    }
+}
+
+// event listeners
+document.getElementById('book-form').addEventListener('submit',
+    function (e) {
+        // get form values
+        const title = document.getElementById('title').value
+        const author = document.getElementById('author').value
+        const isbn = document.getElementById('isbn').value
+
+        const book = new Book(title, author, isbn);
+
+        const ui = new UI();
+
+        ui.addBookToList(book);
+
+        // ui.clearFields()
+
+        e.preventDefault()
+    }
+)
+
+// document.getElementById('clear-list').addEventListener('click',
+//     function () {
+//         const ui = new UI();
+//         ui.clearBookList();
+//     }
+// )
+
